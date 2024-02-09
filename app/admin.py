3117e99082021139dcd56e90276d731aa4f13592
Martin <martin.evenbom@gmail.com>
2024-02-09 11:03:52 +0100

    almost all done

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Products, Category, Customer, Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 # Register your models here.
+admin.site.register(Products,AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,193 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
 
-# Create your views here.
+from django.contrib.auth.hashers import check_password, make_password
+from .models import Customer
+from django.views import View
+from .models import Products, Order, Category
+from store.middlewares.auth import auth_middleware
+
+class Cart(View):
+    def get(self , request):
+        ids = list(request.session.get('cart').keys())
+        products = Products.get_products_by_id(ids)
+        print(products)
+        return render(request , 'cart.html' , {'products' : products} )
+
+class CheckOut(View):
+    def post(self, request):
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        customer = request.session.get('customer')
+        cart = request.session.get('cart')
+        products = Products.get_products_by_id(list(cart.keys()))
+        print(address, phone, customer, cart, products)
+
+        for product in products:
+            print(cart.get(str(product.id)))
+            order = Order(customer=Customer(id=customer),
+                          product=product,
+                          price=product.price,
+                          address=address,
+                          phone=phone,
+                          quantity=cart.get(str(product.id)))
+            order.save()
+        request.session['cart'] = {}
+
+        return redirect('cart')
+
+class Index(View):
+
+    def post(self , request):
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product]  = quantity-1
+                else:
+                    cart[product]  = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart' , request.session['cart'])
+        return redirect('homepage')
+
+
+
+    def get(self , request):
+        # print()
+        return HttpResponseRedirect(f'/store{request.get_full_path()[1:]}')
+
+def store(request):
+    cart = request.session.get('cart')
+    if not cart:
+        request.session['cart'] = {}
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Products.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Products.get_all_products();
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    print('you are : ', request.session.get('email'))
+    return render(request, 'index.html', data)
+
+class Login(View):
+    return_url = None
+
+    def get(self, request):
+        Login.return_url = request.GET.get ('return_url')
+        return render (request, 'login.html')
+
+    def post(self, request):
+        email = request.POST.get ('email')
+        password = request.POST.get ('password')
+        customer = Customer.get_customer_by_email (email)
+        error_message = None
+        if customer:
+            flag = check_password (password, customer.password)
+            if flag:
+                request.session['customer'] = customer.id
+
+                if Login.return_url:
+                    return HttpResponseRedirect (Login.return_url)
+                else:
+                    Login.return_url = None
+                    return redirect ('homepage')
+            else:
+                error_message = 'Invalid !!'
+        else:
+            error_message = 'Invalid !!'
+
+        print (email, password)
+        return render (request, 'login.html', {'error': error_message})
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
+
+class OrderView(View):
+
+
+    def get(self , request ):
+        customer = request.session.get('customer')
+        orders = Order.get_orders_by_customer(customer)
+        print(orders)
+        return render(request , 'orders.html'  , {'orders' : orders})
+
+class Signup (View):
+    def get(self, request):
+        return render (request, 'signup.html')
+
+    def post(self, request):
+        postData = request.POST
+        first_name = postData.get ('firstname')
+        last_name = postData.get ('lastname')
+        phone = postData.get ('phone')
+        email = postData.get ('email')
+        password = postData.get ('password')
+        # validation
+        value = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'phone': phone,
+            'email': email
+        }
+        error_message = None
+
+        customer = Customer (first_name=first_name,
+                             last_name=last_name,
+                             phone=phone,
+                             email=email,
+                             password=password)
+        error_message = self.validateCustomer (customer)
+
+        if not error_message:
+            print(first_name, last_name, phone, email, password)
+            customer.password = make_password(customer.password)
+            customer.register ()
+            return redirect ('homepage')
+        else:
+            data = {
+                'error': error_message,
+                'values': value
+            }
+            return render (request, 'signup.html', data)
+
+    def validateCustomer(self, customer):
+        match True:
+            case _ if not customer.first_name:
+                return "Please Enter your First Name !!"
+            case _ if len(customer.first_name) < 3:
+                return 'First Name must be  3 char long or more'
+            case _ if not customer.last_name:
+                return 'Please Enter your Last Name'
+            case _ if len(customer.last_name) < 3:
+                return 'Last Name must be  3 char long or more'
+            case _ if not customer.phone:
+                return 'Enter your Phone Number'
+            case _ if len(customer.phone) < 10:
+                return 'Phone Number must be  10 char Long'
+            case _ if len(customer.password) < 5:
+                return 'Password must be  5 char long'
+            case _ if len(customer.email) < 5:
+                return 'Email must be  5 char long'
+            case _ if customer.isExists():
+                return 'Email Address Already Registered..'
+            case _:
+                return None
